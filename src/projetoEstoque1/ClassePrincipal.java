a92757a283fcf911eff881f4af68268a35c13e67
gabrielfsilva936 <gabrielfsilva936@gmail.com>
2023-03-04 21:27:20 -0300

    Implementação da classe Gerente

--- src/projetoEstoque1/ClassePrincipal.java
+++ src/projetoEstoque1/ClassePrincipal.java
@@ -1,14 +1,10 @@
 package projetoEstoque1;
-
 public class ClassePrincipal {
-
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         Servicos svc = new Servicos();
-
-        //svc.realizarLogin();
-
-        svc.consultarProduto();
+        svc.realizarLogin();
+        if(svc.getCargo().equals("gerente")){
+            Gerente g1 = new Gerente();
+        }
     }
-
 }
