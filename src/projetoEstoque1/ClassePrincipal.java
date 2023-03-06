cf9124120bedebf8c9e23b3d832b13ce112f68f9
gabrielfsilva936 <gabrielfsilva936@gmail.com>
2023-03-05 23:12:53 -0300

    Finalização da versão 1.0 do software

--- src/projetoEstoque1/ClassePrincipal.java
+++ src/projetoEstoque1/ClassePrincipal.java
@@ -5,6 +5,10 @@ public class ClassePrincipal {
         svc.realizarLogin();
         if(svc.getCargo().equals("gerente")){
             Gerente g1 = new Gerente();
+        }else if(svc.getCargo().equals("repositor")){
+            Repositor r1 = new Repositor();
+        }else if(svc.getCargo().equals("estoquista")){
+            Estoquista e1 = new Estoquista();
         }
     }
 }
